4b78a54052bc3b1bb570cdea3e56430479ff972e
Ali Hamriti <hamritiali@gmail.com>
2021-03-01 16:45:47 +0100

    scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_01_154023) do
+ActiveRecord::Schema.define(version: 2021_03_01_154513) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,4 +27,18 @@ ActiveRecord::Schema.define(version: 2021_03_01_154023) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  create_table "verifications", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone"
+    t.string "name"
+    t.boolean "statut"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_verifications_on_user_id"
+  end
+
+  add_foreign_key "verifications", "users"
 end
